528a96daf0343ea7cafd9d7c0ad96997704e1121
Donatas Remeika <donatas.remeika@gmail.com>
2023-02-03 09:01:13 +0200

    Simple working evaluation

--- brewval/eval.py
+++ brewval/eval.py
@@ -5,11 +5,11 @@ from brewval.model import Prompt, Label
 from langchain.llms import OpenAI, BaseLLM
 
 MODELS: Dict[str, BaseLLM] = {
-    'OpenAi[davinci-003]': OpenAI(model_name='text-davinci-003'),
-    'OpenAi[davinci-002]': OpenAI(model_name='text-davinci-002'),
-    'OpenAi[curie-001]': OpenAI(model_name='text-curie-001'),
-    'OpenAi[babbage-001]': OpenAI(model_name='text-babbage-001'),
-    'OpenAi[ada-001]': OpenAI(model_name='text-ada-001')
+    'OpenAI[davinci-003]': OpenAI(model_name='text-davinci-003'),
+    # 'OpenAI[davinci-002]': OpenAI(model_name='text-davinci-002'),
+    # 'OpenAI[curie-001]': OpenAI(model_name='text-curie-001'),
+    # 'OpenAI[babbage-001]': OpenAI(model_name='text-babbage-001'),
+    'OpenAI[ada-001]': OpenAI(model_name='text-ada-001')
 }
 
 
@@ -17,4 +17,15 @@ class Evaluator:
 
     def evaluate(self, prompt: Prompt, labels: List[Label]) -> None:
         for model in MODELS.keys():
-            print(f'Evaluating PROMPT:\n{prompt.template}\nMODEL: {model}')
+            print(f'Evaluating PROMPT:\n-----\n{prompt.template}\n-----\nMODEL: {model}')
+            match_count: int = 0
+            for label in labels:
+                prompt_text = prompt.get_prompt_from_label(label)
+                output = MODELS[model].generate([prompt_text])
+                result = output.generations[0][0].text
+                matches = label.result_matches(result)
+                print(f'Predicted result "{result}" matches expected "{label.result}": {matches}')
+                if matches:
+                    match_count += 1
+            print(f'Model {model} precision: {100.0 * match_count / len(labels)}%')
+
--- brewval/model.py
+++ brewval/model.py
@@ -1,21 +1,35 @@
-from typing import Dict
+import re
+from typing import Dict, List
+
+
+class Label:
+
+    # TODO: Would make sense to add max expected tokens to save money
+
+    def __init__(self, result: str, parameters: Dict[str, str]):
+        self.result = result
+        self.parameters = parameters
+
+    def result_matches(self, result: str) -> bool:
+        return self.result == result.strip()
 
 
 class Prompt:
 
     def __init__(self, template: str):
+        assert template.endswith('{result}')
         self.template = template
 
-    def get_prompt(self, inputs: Dict[str, str]) -> str:
 
-        # TODO: Replace inputs and remove result placeholders
-        return self.template
+    def get_prompt_from_label(self, label: Label) -> str:
+
+        template = self.template.removesuffix('{result}').strip()
+        placeholders = re.findall(r'(?<=\{)\w+(?=\})', template)
+        for placeholder in placeholders:
+            value = label.parameters[placeholder]
+            template = template.replace('{' + placeholder + '}', value)
+        return template
 
 
 
 
-class Label:
-
-    def __init__(self, result: str, parameters: Dict[str, str]):
-        self.result = result
-        self.parameters = parameters
